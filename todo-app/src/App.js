be53c6e5edc49729da653481cb327372c694abba
eeroleppalehto <eero.leppalehto@gmail.com>
2023-01-15 18:24:38 +0200

    Filters added for ToDos

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,17 +4,14 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
-
 // Parent React Component to be rendered
 function App({ initalTasks }) {
-
   const [tasks, setTasks] = useState(initalTasks);
-
 
   // Callback function to update task list from Form.js
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]);
   }
 
   function toggleTaskCompleted(id) {
@@ -23,57 +20,71 @@ function App({ initalTasks }) {
       if (id === task.id) {
         // use object spread to make a new object
         // whose `completed` prop has been inverted
-        return {...task, completed: !task.completed}
+        return { ...task, completed: !task.completed };
       }
       return task;
     });
     setTasks(updatedTasks);
   }
- 
-  function deleteTask(id){
-    const remainingTasks = tasks.filter( (task) => task.id !== id );
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => task.id !== id);
     setTasks(remainingTasks);
   }
-  
-  function editTask(id, newName){
+
+  function editTask(id, newName) {
     const editedTaskList = tasks.map((task) => {
       // if this task has the same ID as the edited task
-        if (id === task.id) {
-          return {...task, name: newName}
-        }
-        return task;
-      });
-      setTasks(editedTaskList);
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
   }
 
-  
+  const [filter, setFilter] = useState("All");
+
+  const FILTER_MAP = {
+    All: () => true,
+    Active: (task) => !task.completed,
+    Completed: (task) => task.completed,
+  };
+
+  const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 
   // Iterate through tasks list to generate JSX
-  const taskList = tasks.map((item) => (
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((item) => (
     <Todo
-    id={item.id}
-    name={item.name}
-    completed={item.completed}
-    key={item.id}
-    toggleTaskCompleted={toggleTaskCompleted}
-    deleteTask = {deleteTask}
-    editTask = {editTask}
+      id={item.id}
+      name={item.name}
+      completed={item.completed}
+      key={item.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
-    ));
+  ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
 
   // Make the task counter header dynamic
-  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-    
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form addTask={addTask}/>
-      <div className="filters btn-group stack-exception">
-        <FilterButton name="All"/>
-        <FilterButton name="Active"/>
-        <FilterButton name="Completed"/>
-      </div>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
