35942675cc3396daf5b3ff4a3eda96a68322c885
eeroleppalehto <eero.leppalehto@gmail.com>
2023-03-23 12:55:52 +0200

    Refactoring todo api tests

--- todo_backend/tests/todo_api.test.js
+++ todo_backend/tests/todo_api.test.js
@@ -6,154 +6,154 @@ const api = supertest(app)
 
 const Todo = require('../models/todo')
 
-/* const initialTodos =[
-  {
-    name: "VS Code rest client is pretty handy",
-    completed: true,
-    created: "2023-01-22T08:23:62.486Z"
-  },
-  {
-    name: "Test successfully run",
-    completed: false,
-    created: "2023-01-22T08:22:55.486Z"
-  },
-] */
-
-// Initializes test database before tests
-beforeEach(async () => {
-  await Todo.deleteMany({})
-  //console.log('cleared')
-
-  helper.initialTodos.forEach(async (todo) =>{
-    let todoObject = new Todo(todo)
-    await todoObject.save()
-    //console.log('saved')
+describe('when there is initially some todos saved', () => {
+  // Initializes test database before tests
+  beforeEach(async () => {
+    await Todo.deleteMany({})
+    await Todo.insertMany(helper.initialTodos)
   })
-})
 
-// Test for checking if todos are returned as JSON
-test('todos are returned as json', async () => {
-  await api
-    .get('/api/todos')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  // Test for checking if todos are returned as JSON
+  test('todos are returned as json', async () => {
+    await api
+      .get('/api/todos')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-// Test for GET method
-test('all todos are returned', async () => {
-  const response = await api.get('/api/todos')
+  // Test for GET method
+  test('all todos are returned', async () => {
+    const response = await api.get('/api/todos')
 
-  expect(response.body).toHaveLength(helper.initialTodos.length)
-})
+    expect(response.body).toHaveLength(helper.initialTodos.length)
+  })
 
-// Test for checking if a specific note is within the returned todos
-test('a specific todo is within the returned todos', async () => {
-  const response = await api.get('/api/todos')
+  // Test for checking if a specific note is within the returned todos
+  test('a specific todo is within the returned todos', async () => {
+    const response = await api.get('/api/todos')
 
-  const names = response.body.map(r => r.name)
+    const names = response.body.map(r => r.name)
 
-  expect(names).toContain("Test successfully run")
-})
+    expect(names).toContain("Test successfully run")
+  })
 
-// Test for POST method
-test('A valid todo can be added', async () => {
-  const newTodo = {
-    name: 'Try adding a todo (async/await)',
-    completed: false,
-  }
+  describe('addition of new todo', () => {
+    // Test for POST method
+    test('A valid todo can be added', async () => {
+      const newTodo = {
+        name: 'Try adding a todo (async/await)',
+        completed: false,
+      }
 
-  await api
-    .post('/api/todos')
-    .send(newTodo)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/todos')
+        .send(newTodo)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const response = await helper.todosInDb()
-  expect(response).toHaveLength(helper.initialTodos.length + 1)
+      const response = await helper.todosInDb()
+      expect(response).toHaveLength(helper.initialTodos.length + 1)
 
-  const names = response.map(r => r.name)
-  expect(names).toContain('Try adding a todo (async/await)')
+      const names = response.map(r => r.name)
+      expect(names).toContain('Try adding a todo (async/await)')
 
-})
+    })
 
-// Test for checking if Todo with no name is added
-test('Todo without name is not added', async () => {
-  const newTodo = {
-    completed: false
-  }
+    // Test for checking if Todo with no name is added
+    test('Todo without name is not added', async () => {
+      const newTodo = {
+        completed: false
+      }
 
-  await api
-    .post('/api/todos')
-    .send(newTodo)
-    .expect(400)
+      await api
+        .post('/api/todos')
+        .send(newTodo)
+        .expect(400)
 
-  const todosAtEnd = await helper.todosInDb()
+      const todosAtEnd = await helper.todosInDb()
 
-  expect(todosAtEnd).toHaveLength(helper.initialTodos.length)
-})
+      expect(todosAtEnd).toHaveLength(helper.initialTodos.length)
+    })
+  })
 
-// Test to get a specific todo
-test('a specific todo can be viewed', async () => {
-  /* const todos = await Todo.find({})
+  describe('viewing a specific todo', () => {
+    // Test to get a specific todo
+    test('a specific todo can be viewed', async () => {
+      const todos = await helper.todosInDb()
 
-  const todosJSON = todos.map(todo => todo.toJSON()) */
+      const todoToView = todos[0]
 
-  const todos = await helper.todosInDb()
+      const resultTodo = await api
+        .get(`/api/todos/${todoToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+      
+      expect(resultTodo.body).toEqual(todoToView)
+    })
 
-  const todoToView = todos[0]
+    test('fails with status code 404 if todo does not exist', async () =>{
+      const validNonexistingId = await helper.nonExistingId()
 
-  const resultTodo = await api
-    .get(`/api/todos/${todoToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  
-  expect(resultTodo.body).toEqual(todoToView)
-})
+      await api
+        .get(`/api/todos/${validNonexistingId}`)
+        .expect(404)
+    })
 
-// Test for deleting a todo
-test('a todo can be deleted', async () => {
-  /* const todos = await Todo.find({})
-  const todosJSON = todos.map(todo => todo.toJSON()) */
+    test('fails with statuscode 400 id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
 
-  const todos = await helper.todosInDb()
+      await api
+        .get(`/api/todos/${invalidId}`)
+        .expect(400)
+    })
+  })
 
-  const todoToDelete = todos[0]
+  // Test for deleting a todo
+  test('a todo can be deleted', async () => {
+    /* const todos = await Todo.find({})
+    const todosJSON = todos.map(todo => todo.toJSON()) */
 
-  await api
-    .delete(`/api/todos/${todoToDelete.id}`)
-    .expect(204)
+    const todos = await helper.todosInDb()
 
-  const newTodos = await Todo.find({})
-  const newTodosJSON = newTodos.map(todo => todo.toJSON())
+    const todoToDelete = todos[0]
 
-  expect(newTodosJSON).toHaveLength(helper.initialTodos.length - 1)
-
-  const names = newTodosJSON.map(r => r.name)
-
-  expect(names).not.toContain(todoToDelete.name)
-})
-
-// Test for updating a todo
-test('a todo can be updated', async () => {
-  /* const todos = await Todo.find({})
-  const todosJSON = todos.map(todo => todo.toJSON()) */
-
-  const todos = await helper.todosInDb()
-
-  const todoToUpdate = todos[0]
-  todoToUpdate.name = 'Name field has been updated'
-
-  await api
-    .put(`/api/todos/${todoToUpdate.id}`)
-    .send(todoToUpdate)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .delete(`/api/todos/${todoToDelete.id}`)
+      .expect(204)
 
     const newTodos = await Todo.find({})
-    const names = newTodos.map(r => r.name)
+    const newTodosJSON = newTodos.map(todo => todo.toJSON())
 
-    expect(names).toContain('Name field has been updated')
+    expect(newTodosJSON).toHaveLength(helper.initialTodos.length - 1)
+
+    const names = newTodosJSON.map(r => r.name)
+
+    expect(names).not.toContain(todoToDelete.name)
+  })
+
+  // Test for updating a todo
+  test('a todo can be updated', async () => {
+    /* const todos = await Todo.find({})
+    const todosJSON = todos.map(todo => todo.toJSON()) */
+
+    const todos = await helper.todosInDb()
+
+    const todoToUpdate = todos[0]
+    todoToUpdate.name = 'Name field has been updated'
+
+    await api
+      .put(`/api/todos/${todoToUpdate.id}`)
+      .send(todoToUpdate)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+      const newTodos = await Todo.find({})
+      const names = newTodos.map(r => r.name)
+
+      expect(names).toContain('Name field has been updated')
 })
+})
+
 
 // Closes connection to MongoDB after tests are run
 afterAll(async () => {
