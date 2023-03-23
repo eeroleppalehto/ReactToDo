e87446aa8f6dd1dc93a590fe0db1978adf6f5313
eeroleppalehto <eero.leppalehto@gmail.com>
2023-03-23 12:50:11 +0200

    Added imports for login router and async-errors

--- todo_backend/app.js
+++ todo_backend/app.js
@@ -2,10 +2,12 @@
 
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const todosRouter = require('./controllers/todos')
 const usersRouter = require('./controllers/users')
+const loginRouter =require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -30,6 +32,7 @@ app.use(morgan('tiny'))
 
 app.use('/api/todos', todosRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
