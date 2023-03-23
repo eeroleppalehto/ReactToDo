33e2f3cba529d52df21225d011ce0fd04bffd03c
eeroleppalehto <eero.leppalehto@gmail.com>
2023-03-23 12:54:35 +0200

    Added GET and DELETE methods

--- todo_backend/controllers/users.js
+++ todo_backend/controllers/users.js
@@ -1,7 +1,22 @@
 const bcrypt = require('bcrypt')
-const { request } = require('../app')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+
+usersRouter.get('/', async (req,res) => {
+    const users = await User
+        .find({}).populate('todos', { name: 1, completed: 1})
+    res.json(users)
+})
+
+usersRouter.get('/:id', async (req,res) => {
+    const userId = req.params.id
+    const user = await User.findById(userId)
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404).end()
+    }
+})
 
 usersRouter.post('/', async (req, res) => {
     const{ username, name, password } = req.body
@@ -18,6 +33,11 @@ usersRouter.post('/', async (req, res) => {
     const savedUser = await user.save()
 
     res.status(201).json(savedUser)
+})
+
+usersRouter.delete('/:id', async (req, res) => {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(204).end()
 })
 
 module.exports = usersRouter
