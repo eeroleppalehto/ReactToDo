a89d0a34f80431e8ac9c41934af30ecbc6c6c026
eeroleppalehto <eero.leppalehto@gmail.com>
2023-03-28 13:59:03 +0300

    eslint + suggested fixes

--- todo-app/src/utils/SortFunctions.js
+++ todo-app/src/utils/SortFunctions.js
@@ -1,43 +1,43 @@
 // Sorting functions to sort tasks in user interface
 
 const az_sort = (itemA, itemB) => {
-    const nameA = itemA.name.toUpperCase();
-    const nameB = itemB.name.toUpperCase();
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-        return 1;
-    }
+  const nameA = itemA.name.toUpperCase()
+  const nameB = itemB.name.toUpperCase()
+  if (nameA < nameB) {
+    return -1
+  }
+  if (nameA > nameB) {
+    return 1
+  }
 
-    return 0;
-};
+  return 0
+}
 
 const za_sort = (itemAr, itemBr) => {
-    const nameA = itemAr.name.toUpperCase();
-    const nameB = itemBr.name.toUpperCase();
-    if (nameA < nameB) {
-        return 1;
-    }
-    if (nameA > nameB) {
-        return -1;
-    }
+  const nameA = itemAr.name.toUpperCase()
+  const nameB = itemBr.name.toUpperCase()
+  if (nameA < nameB) {
+    return 1
+  }
+  if (nameA > nameB) {
+    return -1
+  }
 
-    return 0;
-};
+  return 0
+}
 
 const oldest_sort = (a, b) => {
-    return new Date(a.created).valueOf() - new Date(b.created).valueOf();
-};
+  return new Date(a.created).valueOf() - new Date(b.created).valueOf()
+}
 
 const newest_sort = (a, b) => {
-    return new Date(b.created).valueOf() - new Date(a.created).valueOf();
-};
+  return new Date(b.created).valueOf() - new Date(a.created).valueOf()
+}
 
 
 export default {
-    "az": az_sort,
-    "za": za_sort,
-    "oldest": oldest_sort,
-    "newest": newest_sort
+  'az': az_sort,
+  'za': za_sort,
+  'oldest': oldest_sort,
+  'newest': newest_sort
 }
