24b7ac44f7a0c35b803bb7360a5a5829a6c00f7f
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-01 11:26:07 +0300

    Added testing router

--- todo_backend/app.js
+++ todo_backend/app.js
@@ -5,9 +5,11 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const todosRouter = require('./controllers/todos')
 const usersRouter = require('./controllers/users')
-const loginRouter =require('./controllers/login')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -18,12 +20,12 @@ mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+.then(() => {
+  logger.info('connected to MongoDB')
+})
+.catch((error) => {
+  logger.error('error connection to MongoDB:', error.message)
+})
 
 app.use(cors())
 app.use(express.static('build'))
@@ -33,6 +35,12 @@ app.use(morgan('tiny'))
 app.use('/api/todos', todosRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
